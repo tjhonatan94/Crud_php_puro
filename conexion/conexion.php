52817841b9771fd0698e1d4099aae742efc9c500
tjhonatan94 <tjhonatan@unicauca.edu.co>
2021-12-23 15:04:04 -0500

    Conexion a la base de datos POD

---
+++ conexion/conexion.php
@@ -0,0 +1,17 @@
+<?php
+
+$servidor="mysql:dbname=empresa;host=127.0.0.0.1";
+$usuario="root";
+$password="";
+
+try{
+
+    $pdo= new PDO($servidor, $usuario, $password);
+    echo "Conectado...";
+}catch(PDOException $e){
+
+    echo "Conexión mala :( ".$e->getMessage();
+
+}
+?>
+
